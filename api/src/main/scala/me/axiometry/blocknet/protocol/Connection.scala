09f459a7f8d070801f0f8ec853b2c86f90f27576
Axiometry <marco@vanhil.st>
2015-01-12 13:56:45 -0500

    Abstract type members don't work here

--- api/src/main/scala/me/axiometry/blocknet/protocol/Connection.scala
+++ api/src/main/scala/me/axiometry/blocknet/protocol/Connection.scala
@@ -5,31 +5,26 @@ import java.net._
 
 import me.axiometry.blocknet.ServerAddress
 
-trait Connection {
-  type In <: InputStream
-  type Out <: OutputStream
-
-  def in: In
-  def out: Out
+trait Connection[I <: InputStream, O <: OutputStream] {
+  def in: I
+  def out: O
 
   def connected: Boolean
   def disconnect()
 }
 
 object Connection {
-  protected[this] case class SocketConnection[I <: InputStream, O <: OutputStream](val socket: Socket, val in: I, val out: O) extends Connection {
-    override type In = I
-    override type Out = O
+  protected[this] case class SocketConnection[I <: InputStream, O <: OutputStream](socket: Socket, in: I, out: O) extends Connection[I, O] {
     override def connected = socket.isConnected && !socket.isClosed
     override def disconnect() = socket.close
   }
-  def apply[I <: InputStream, O <: OutputStream](socket: Socket, createIn: InputStream => I, createOut: OutputStream => O): Connection = {
+  def apply[I <: InputStream, O <: OutputStream](socket: Socket, createIn: InputStream => I, createOut: OutputStream => O): Connection[I, O] = {
     val conn = SocketConnection(socket, createIn(socket.getInputStream), createOut(socket.getOutputStream))
     if(!conn.connected)
       throw new IOException("Unconnected socket")
     conn
   }
-  def apply[I <: InputStream, O <: OutputStream](address: ServerAddress, createIn: InputStream => I, createOut: OutputStream => O): Connection = {
+  def apply[I <: InputStream, O <: OutputStream](address: ServerAddress, createIn: InputStream => I, createOut: OutputStream => O): Connection[I, O] = {
     val socket = new Socket(address.host, address.port)
     apply(socket, createIn, createOut)
   }
