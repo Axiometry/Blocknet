e3761a6c93a13bde37edc14ee88839abacc6bdd9
Axiometry <marco@vanhil.st>
2015-05-06 02:55:13 -0400

    Lay out Block structure

--- api/src/main/scala/me/axiometry/blocknet/block/Block.scala
+++ api/src/main/scala/me/axiometry/blocknet/block/Block.scala
@@ -1,18 +1,42 @@
 package me.axiometry.blocknet.block
 
+import scala.util.Try
+
+import BlockFace._
+
 import me.axiometry.blocknet._
+import me.axiometry.blocknet.item.Item
 
-trait Block extends BlockLocatable {
-  def world: World
-  def chunk: Chunk
+abstract class Block(val id: Int, val name: String)
 
-  def id: Int
-  def meta: Int
-  def light: Int
-  def tileEntity: TileEntity
+object Block {
+  trait ToolType[T <: Item.Tool] extends Block
+  trait Strength extends Block {
+    def strength: Int
+  }
 
-  def id_=(id: Int)
-  def meta_=(meta: Int)
-  def light_=(light: Int)
-  def tileEntity_=(tileEntity: TileEntity)
+  trait Data {
+    def value: Int
+  }
+  trait ModelData {
+    def model: Model = ModelData.defaultModel
+  }
+  object ModelData {
+    private[Block] val defaultModel = Model(List(BoundingBox(0, 0, 0, 1, 1, 1)))
+
+    trait Solid extends ModelData
+    trait Interactible extends ModelData
+  }
+
+  trait DataType extends Block {
+    type Data <: Block.Data
+    def createData(): Data = createData(0).get
+    def createData(value: Int): Option[Data]
+  }
+
+  sealed case class UnspecifiedData(val value: Int) extends Data
+  trait UnspecifiedDataType extends DataType {
+    override type Data = UnspecifiedData
+    override def createData(value: Int) = Option(UnspecifiedData(value))
+  }
 }
