3251f5eaf0142dbd80838c1425725068b191eee0
Axiometry <marco@vanhil.st>
2015-05-06 22:27:50 -0400

    Server changes

--- api/src/main/scala/me/axiometry/blocknet/Server.scala
+++ api/src/main/scala/me/axiometry/blocknet/Server.scala
@@ -4,8 +4,18 @@ import me.axiometry.blocknet._
 
 case class ServerAddress(host: String, port: Int)
 trait Server {
-  val address: ServerAddress
+  def address: ServerAddress
 
   def worlds: Set[World]
   def createBot(): Bot
+}
+object Server {
+  val DEFAULT_PORT = 25565
+}
+package object blocknet {
+  private[this] val AddressPattern = "([^:]+)(:([0-9]{1,5}))?".r
+  implicit def string2Address(desc: String) = desc match {
+    case AddressPattern(host, _, port) => ServerAddress(host, port.toInt)
+    case AddressPattern(host, x, y) => println(s"$x / $y"); ServerAddress(host, 25565)
+  }
 }
